f5d12bc576458f6386bc4e45c8055a3760b2e782
Cosillow <silloc@rpi.edu>
2022-03-01 00:19:53 -0500

    Created api endpoints for dorms and dorms/:dorm for use by frontend

--- backend/server.js
+++ backend/server.js
@@ -2,40 +2,47 @@
 var app = require('express')();
 var http = require('http').Server(app);
 const axios = require('axios').default;
+const fs = require('fs');
+
+// This will be mongo in the future:
+var dorms;
+fs.readFile('./dorms.json', 'utf8', (err, data) => {
+  if (err) {
+      console.log(`Error reading file from disk: ${err}`);
+  } else {
+    // parse JSON string to JSON object
+    dorms = JSON.parse(data);
+  }
+});
+
+
+
 
 // server main route handler
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 // server dorm route handler
 app.get('/dorms', (req, res) => {
-    res.status(200).json({
-      "result": true
-    });
+  var arr = [];
+  for(var key in dorms["dorms"]) {
+    var dorm = dorms["dorms"][key];
+    arr.push(dorm["name"]);
+  }
+  res.status(200).json({"dorms": arr})
 });
 
-// server dorm itme route handler
-app.get('/dorms/:q', (req, res) => {
-  switch(q) {
-    case "name":
-      res.status(200).json({
-        "result": true
-      });
-      break;
-    case "price":
-      res.status(200).json({
-        "result": true
-      });
-      break;
-    case "reviews":
-      res.status(200).json({
-        "result": true
-      });
-      break;
-    default:
-      break;
+// server dorm item route handler
+app.get('/dorms/:dorm', (req, res) => {
+  // can be called by either the common or official dorm name
+  for(var key in dorms["dorms"]) {
+    var dorm = dorms["dorms"][key];
+    if (dorm.name.common === req.params.dorm || dorm.name.official === req.params.dorm) {
+      res.status(200).json(dorm);
+    }
   }
+  res.status(404).send();
 });
 
 app.put('/dorms/reviews/:id', (req, res) => {
@@ -52,5 +59,5 @@ app.delete('/dorms/reviews/:id', (req, res) => {
 
 // start server
 http.listen(3000, function () {
-    console.log('Server up on *:3000');
+  console.log('Server up on *:3000');
 });
