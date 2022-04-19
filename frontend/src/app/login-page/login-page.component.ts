ef4ea63f5129dd367688ba5266a7327c314419ef
bhavesh42 <bajajb@rpi.edu>
2022-04-19 16:06:12 -0400

    Google Login (Firebase)
    Implemented login with Google using Firebase

--- frontend/src/app/login-page/login-page.component.ts
+++ frontend/src/app/login-page/login-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpService } from '../http.service';
 import { UserService } from '../user.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -12,12 +14,18 @@ export class LoginPageComponent implements OnInit {
   username: string = "";
   password: string = "";
 
-  constructor(private http: HttpService, private user: UserService, private router: Router) { }
+  constructor(
+    private http: HttpService,
+    private user: UserService,
+    private router: Router,
+    public authService: AuthService,
+    public auth: AngularFireAuth
+  ) { }
 
   ngOnInit(): void {
   }
 
-  login() {
+  loginRegular() {
     console.log(`${this.username} ${this.password}`);
     if(this.username !== "" && this.password !== "") {
       this.http.get(`/login?uname=${this.username}&pass=${this.password}`).subscribe((data: any) => {
